81070d5bda1422c26a63e1bd580cb010b583f44f
Giovan <giovanmail@gmail.com>
2016-08-07 15:27:26 +0700

    First Attempt implementing Jerry Nixon's style SplitView menu

--- GMD/MainPage.xaml.cs
+++ GMD/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMD.ViewModels;
+using GMD.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +27,94 @@ namespace GMD
         public MainPage()
         {
             this.InitializeComponent();
+            MenuFrame.Navigate(typeof(SearchPage));
+            var update = new Action(() =>
+            {
+                // update radiobuttons after frame navigates
+                var type = MenuFrame.CurrentSourcePageType;
+                foreach (var radioButton in AllRadioButtons(this))
+                {
+                    var target = radioButton.CommandParameter as NavType;
+                    if (target == null)
+                        continue;
+                    radioButton.IsChecked = target.Type.Equals(type);
+                }
+                HamburgerSplitView.IsPaneOpen = false;
+                BackCommand.RaiseCanExecuteChanged();
+            });
+            MenuFrame.Navigated += (s, e) => update();
+            Loaded += (s, e) => update();
+            DataContext = this;
         }
+
+        Command _backCommand;
+        public Command BackCommand
+        {
+            get { return _backCommand ?? (_backCommand = new ViewModels.Command(ExecuteBack, CanBack)); }
+        }
+        private bool CanBack()
+        {
+            return MenuFrame.CanGoBack;
+        }
+        private void ExecuteBack()
+        {
+            MenuFrame.GoBack();
+        }
+
+        private void HamburgerMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExecuteMenu();
+        }
+        private void ExecuteMenu()
+        {
+            HamburgerSplitView.IsPaneOpen = !HamburgerSplitView.IsPaneOpen;
+        }
+
+        // nav
+        Command<NavType> _navCommand;
+        public Command<NavType> NavCommand { get { return _navCommand ?? (_navCommand = new Command<NavType>(ExecuteNav)); } }
+        private void ExecuteNav(NavType navType)
+        {
+            MenuFrame.Navigate(navType.Type);
+            
+            // when we nav home, clear history
+            if (navType.Type.Equals(typeof(SearchPage)))
+                MenuFrame.BackStack.Clear();
+
+            // navigate only to new pages
+            if (MenuFrame.Content != null && MenuFrame.Content.GetType() != navType.Type)
+                MenuFrame.Navigate(navType.Type, navType.Parameter);
+        }
+
+        // utility
+        public List<RadioButton> AllRadioButtons(DependencyObject parent)
+        {
+            var list = new List<RadioButton>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is RadioButton)
+                {
+                    list.Add(child as RadioButton);
+                    continue;
+                }
+                list.AddRange(AllRadioButtons(child));
+            }
+            return list;
+        }
+
+        // prevent check
+        private void DontCheck(object s, RoutedEventArgs e)
+        {
+            // don't let the radiobutton check
+            (s as RadioButton).IsChecked = false;
+        }        
     }
+
+    public class NavType
+    {
+        public Type Type { get; set; }
+        public string Parameter { get; set; }
+    }
+
 }
