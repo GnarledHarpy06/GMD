85aedd1c18822b136758c8e1e9b625fad62b17d6
Giovan Isa Musthofa <giovanisamusthofa@outlook.com>
2016-10-30 18:43:07 +0700

    Dict class and their constructor added, not tested yet. Partially ManageDicts class.

---
+++ GMD/ViewModels/Dict.cs
@@ -0,0 +1,181 @@
+﻿using SharpCompress.Readers;
+using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Windows.Storage;
+
+namespace GMD.ViewModels
+{
+    class Dict
+    {
+        [PrimaryKey, AutoIncrement]
+        private int dictID { get; set; }
+        [MaxLength(64)]
+        public string FileName { get; private set; }
+        public string Directory { get; private set; }
+
+        public string BookName { get; private set; }
+        public int WordCount { get; private set; }
+        public int IdxFileSize { get; private set; }
+        public string Author { get; private set; }
+        public string Email { get; private set; }
+        public string Website { get; private set; }
+        public string Description { get; private set; }
+        public string Date { get; private set; }
+
+        public int synWordCount { get; private set; } // unsupported
+        public byte idxOffsetBits { get; private set; }
+        public string dictType { get; private set; }
+        public string sameTypeSequence { get; private set; }
+
+        private string ifoPath { get; set; }
+        private string dictPath { get; set; }
+        private string idxPath { get; set; }
+        private string synPath { get; set; }
+
+        private string ifoOftPath { get; set; }
+        private string synOftPath { get; set; }
+
+        // private string ifoCltPath { get; set; } // unsupported
+        // private string synCltPath { get; set; } // unsupported
+
+        private Dict() { }
+
+        private Dict(string fileName)
+        {
+            FileName = fileName;
+            Directory = Path.Combine
+                (ApplicationData.Current.LocalFolder.Path, FileName);
+        }
+
+        public async void BuildDictionaryAsync()
+        {
+            try
+            {
+                ifoPath = getPathByFileType(".ifo");
+                synPath = getPathByFileType(".syn");
+
+                ifoOftPath = getPathByFileType(".ifo.oft");
+                synOftPath = getPathByFileType(".syn.oft");
+            }
+            finally { }
+
+            try
+            { dictPath = getPathByFileType(".dict"); }
+            catch
+            {
+                try
+                { extract("dict.dz"); }
+                catch { extract("dict.gz"); }
+
+                dictPath = getPathByFileType(".dict");
+            }
+
+            try
+            { idxPath = getPathByFileType(".idx"); }
+            catch
+            {
+                extract("idx.gz");
+
+                idxPath = getPathByFileType(".idx");
+            }
+
+            List<String> lines;
+
+            using (MemoryStream memStream = new MemoryStream())
+            using (Stream stream = await
+                (await StorageFile.GetFileFromPathAsync(ifoPath))
+                .OpenStreamForReadAsync())
+            {
+                await stream.CopyToAsync(memStream);
+                byte[] _byteBuffer = memStream.ToArray();
+                string ifo = Encoding.UTF8.GetString(_byteBuffer, 0, _byteBuffer.Length);
+                lines = ifo.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                
+                /*  This code reads data from .ifo file as a stream
+                 *  copys them to a memory stream
+                 *  turns them into byteArray
+                 *  turns them into a single string
+                 *  assign each line to lines List<String>
+                 *  and dispose the unused stream, memStream, and string from memory
+                 */
+            }
+
+            try
+            {
+                BookName = getStringValue(lines, "bookname");
+                WordCount = int.Parse(getStringValue(lines, "wordcount"));
+                IdxFileSize = int.Parse(getStringValue(lines, "idxfilesize"));
+            }
+            catch { BookName = FileName; }
+
+            try { idxOffsetBits = byte.Parse(getStringValue(lines, "idxoffsetbits")); }
+            catch { }
+
+            try { synWordCount = int.Parse(getStringValue(lines, "synwordcount")); }
+            catch { }
+
+            try { Author = getStringValue(lines, "author"); }
+            catch { }
+
+            try { Email = getStringValue(lines, "email"); }
+            catch { }
+
+            try { Website = getStringValue(lines, "website"); }
+            catch { }
+
+            try { Description = getStringValue(lines, "description"); }
+            catch { }
+
+            try { Date = getStringValue(lines, "date"); }
+            catch { }
+
+            try { sameTypeSequence = getStringValue(lines, "sametypesequence"); }
+            catch { }
+
+            try { dictType = getStringValue(lines, "dicttype"); }
+            catch { }
+        }
+
+        private static string getStringValue(List<string> lines, string valueType)
+        {
+            return lines.Select(line => line.Contains($"{valueType}=") ?
+                    line.Remove(0, line.Length - $"{valueType}=".Length) : null)
+                    .ToString();
+        }
+
+        private string getPathByFileType(string extension)
+        {
+            var files = System.IO.Directory.EnumerateFiles(Directory, extension, SearchOption.TopDirectoryOnly);
+
+            foreach (string currentFile in files)
+            {
+                string _fileName = currentFile.Remove(0, currentFile.Length - extension.Length);
+                if (_fileName == extension)
+                {
+                    return _fileName;
+                }
+            }
+            return null;
+        }
+        
+        private async void extract(string extension)
+        {
+            string path = getPathByFileType(extension);
+            StorageFile file = await StorageFile.GetFileFromPathAsync(path);
+
+            using (Stream stream = await file.OpenStreamForReadAsync())
+            using (var reader = ReaderFactory.Open(stream))
+            {
+                reader.WriteEntryToDirectory(Directory, new ExtractionOptions()
+                {
+                    ExtractFullPath = true,
+                    Overwrite = true
+                });
+            }
+        }
+    }
+}
---
+++ GMD/ViewModels/ManageDicts.cs
@@ -0,0 +1,84 @@
+﻿using SharpCompress.Archives;
+using SharpCompress.Common;
+using SharpCompress.Readers;
+using SharpCompress.Writers;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace GMD.ViewModels
+{
+    public class ManageDicts
+    {
+        private static string path = Path.Combine
+            (ApplicationData.Current.LocalFolder.Path, "Dicts_db.sqlite");
+
+        private SQLiteConnection connection = ManageDicts.GetDatabaseConnection();
+
+        private static SQLiteConnection GetDatabaseConnection()
+        {
+            SQLiteConnection _connection;
+
+            if (!System.IO.File.Exists(path))
+            {
+                _connection = new SQLiteConnection
+                    (new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+                _connection.CreateTable<Dict>();
+            }
+            else
+            {
+                _connection = new SQLiteConnection
+                    (new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);                
+            }
+
+            return _connection;
+        }
+
+        private async Task<StorageFile> selectAFileAsync()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.List;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".tar");
+            openPicker.FileTypeFilter.Add(".gz");
+            openPicker.FileTypeFilter.Add(".bz2");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                return file;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        async void AddDict()
+        {
+            StorageFile file = await selectAFileAsync();
+
+            
+            using (Stream stream = await file.OpenStreamForReadAsync())
+            using (var reader = ReaderFactory.Open(stream))
+            {
+                while (reader.MoveToNextEntry())
+                {
+                    if (!reader.Entry.IsDirectory)
+                    {
+                        reader.WriteEntryToDirectory(ApplicationData.Current.LocalFolder.Path, new ExtractionOptions()
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = true
+                        });
+                    }
+                }
+            }
+        }
+    }    
+}
